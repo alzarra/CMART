0a0de25e6aff32f854b9b7c1f07b53bcad80703c
KMANK88 <karstenpease88@outlook.com>
2019-02-27 17:29:24 -0700

    added database stuff

---
+++ Project/database.js
@@ -0,0 +1,17 @@
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "127.0.0.1",
+  user: "root",
+  password: 'csci427',
+  database: 'mysql'
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  con.query("SELECT * FROM mrTable", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+  });
+});
